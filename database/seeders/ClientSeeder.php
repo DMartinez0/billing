805d719bd9eb712a573bd8698194b93bac49cb32
Erick Nuñez <42759691+aericknunez@users.noreply.github.com>
2024-01-12 20:31:51 -0600

    Client Seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,27 @@ class ClientSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Client::create([
+            'nit' => '02072103861029',
+            'ncr' => '2800134',
+            'nombre' => 'Erick Adonai Nuñez Martinez',
+            'cod_actividad' => '63990',
+            'desc_actividad' => 'Programación informatica',
+            'nombre_comercial' => 'Hibrido El Salvador',
+            'tipo_establecimiento' => "02",
+            'direccion_departamento' => "02",
+            'direccion_municipio' => "07",
+            'direccion_complemento' => 'Las Americas 1, Pol I # 4',
+            'telefono' => '60623882',
+            'correo' => 'aerick.nunez@gmail.com',
+            'cod_estable_mh' => null,
+            'cod_estable' => null,
+            'cod_punto_venta_mh' => null,
+            'cod_punto_venta' => null,
+            'user_id' => 1,
+            'status' => 1,
+        ]);
     }
 }
